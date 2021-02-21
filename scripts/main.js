18b271b5d29fbc585f17ba6f25ef929ac1c80996
David Osorio <davidosorioleyva@outlook.com>
2021-02-20 23:44:50 -0600

    Archivos listos

---
+++ scripts/main.js
@@ -0,0 +1,136 @@
+const celeste = document.getElementById('celeste')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde = document.getElementById('verde')
+const btnEmpezar = document.getElementById('btnEmpezar')
+const ULTIMO_NIVEL = 10
+
+class Juego {
+    constructor() {
+        this.inicializar = this.inicializar.bind(this)
+        this.inicializar()
+        this.generarSecuencia()
+        setTimeout(this.siguienteNivel, 800)
+    }
+    inicializar() {
+        this.siguienteNivel = this.siguienteNivel.bind(this)
+        this.elegirColor = this.elegirColor.bind(this)
+        this.toggleBtnEmpezar()
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+    toggleBtnEmpezar() {
+        if (btnEmpezar.classList.contains('hide')) {
+            btnEmpezar.classList.remove('hide')
+        } else {
+            btnEmpezar.classList.add('hide')
+        }
+    }
+    generarSecuencia() {
+        this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(() => Math.floor(Math.random() * 4))
+    }
+    cambiarNivel() {
+        if (this.nivel === 1) {
+            this.siguienteNivel()
+        } else {
+            swal('Buen trabajo', `Haz completado el nivel ${this.nivel - 1}`, 'success')
+            .then(this.siguienteNivel)
+        }
+    }
+    siguienteNivel() {
+        this.subnivel = 0
+        this.iluminarSecuencia()
+        this.agregarEventosClick()
+    }
+    numeroAColor(num) {
+        switch (num) {
+            case 0:
+                return 'celeste'
+            case 1:
+                return 'violeta'
+            case 2:
+                return 'naranja'
+            case 3:
+                return 'verde'
+        }
+    }
+    colorANumero(col) {
+        switch (col) {
+            case 'celeste':
+                return 0
+            case 'violeta':
+                return 1
+            case 'naranja':
+                return 2
+            case 'verde':
+                return 3
+        }
+    }
+    iluminarSecuencia() {
+        for (let i = 0; i < this.nivel; i++) {
+            const color = this.numeroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), 1000 * i)
+        }
+    }
+    iluminarColor(color) {
+        this.colores[color].classList.add('light')
+        setTimeout(() => this.apagarColor(color), 350)
+    }
+    apagarColor(color) {
+        this.colores[color].classList.remove('light')
+    }
+    agregarEventosClick() {
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
+    }
+    eliminarEventosClick() {
+        this.colores.celeste.removeEventListener('click', this.elegirColor)
+        this.colores.violeta.removeEventListener('click', this.elegirColor)
+        this.colores.naranja.removeEventListener('click', this.elegirColor)
+        this.colores.verde.removeEventListener('click', this.elegirColor)
+    }
+    elegirColor(ev) {
+        const nombreColor = ev.target.dataset.color
+        const numeroColor = this.colorANumero(nombreColor)
+        this.iluminarColor(nombreColor)
+        if (numeroColor === this.secuencia[this.subnivel]) {
+            this.subnivel++
+            if (this.subnivel === this.nivel) {
+                this.nivel++
+                this.eliminarEventosClick()
+                if (this.nivel === (ULTIMO_NIVEL + 1)) {
+                    // Gano!
+                    this.ganoElJuego()
+                } else {
+                    //swal('Bien', 'Pasate el nivel', 'success')
+                    setTimeout(this.cambiarNivel(), 1500)
+                }
+            }
+        } else {
+            // Perdió!
+            this.perdioElJuego()
+        }
+    }
+    ganoElJuego() {
+        swal('Felicidades', 'Ganaste el juego!', 'success')
+            .then(this.inicializar)
+    }
+    perdioElJuego() {
+        swal('Ni modo', 'Lo siento perdiste :(', 'error')
+        .then(() => {
+            this.eliminarEventosClick()
+            this.inicializar()
+        })
+    }
+}
+
+function empezarJuego() {
+    window.juego = new Juego()
+}
